267309b9b59ccc68eef1da29203233efb67e2671
r3n3gader <152099391+r3n3gader@users.noreply.github.com>
2024-09-01 09:59:51 +0200

    Zippen in build-funktion eingebaut.

--- Build.bat
+++ Build.bat
@@ -1,5 +1,14 @@
 @echo off
 
+:: Schritt 0: Überprüfen, ob Docker Engine läuft
+echo Checking if Docker Engine is running...
+docker info >nul 2>&1
+if %ERRORLEVEL% NEQ 0 (
+    echo Docker Engine is not running. Please start Docker and try again.
+    exit /b %ERRORLEVEL%
+)
+echo Docker Engine is running.
+
 :: Schritt 1: BuildFatJar Task ausführen
 echo Running buildFatJar...
 call gradlew buildFatJar
@@ -8,7 +17,7 @@ if %ERRORLEVEL% NEQ 0 (
     exit /b %ERRORLEVEL%
 )
 
-:: Schritt 2: Docker-Image bauen
+:: Schritt 4: Docker-Image bauen
 echo Building Docker image...
 docker build --no-cache -t datenanalysetool:dev-latest -f ./deploy/Dockerfile .
 if %ERRORLEVEL% NEQ 0 (
@@ -16,8 +25,8 @@ if %ERRORLEVEL% NEQ 0 (
     exit /b %ERRORLEVEL%
 )
 
-:: Schritt 3: Docker-Image speichern
-echo Docker image saving...
+:: Schritt 5: Docker-Image speichern
+echo Saving Docker image...
 docker save -o ./deploy/datenanalysetool.tar datenanalysetool:dev-latest
 if %ERRORLEVEL% NEQ 0 (
     echo Docker save failed. Exiting...
@@ -25,4 +34,17 @@ if %ERRORLEVEL% NEQ 0 (
 )
 echo Docker image saved as datenanalysetool.tar
 
-pause
+:: Schritt 4: Den gesamten deploy-Ordner zippen
+echo Zipping the deploy directory...
+if exist deploy.zip (del deploy.zip)
+powershell -Command "Compress-Archive -Path deploy\* -DestinationPath deploy.zip"
+if %ERRORLEVEL% NEQ 0 (
+    echo Failed to zip the deploy directory. Exiting...
+    exit /b %ERRORLEVEL%
+)
+echo Deploy directory zipped as deploy.zip
+
+
+
+
+
